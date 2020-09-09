72ab303f2abfa9398eb12ebd18be98eb67814488
chenxx <chenxx@michi-tecm.com>
2020-09-09 17:25:30 +0800

    add: DruidSegmentmetadataQuery

--- src/main/java/in/zapr/druid/druidry/query/metadata/DruidSegmentMetadataQuery.java
+++ src/main/java/in/zapr/druid/druidry/query/metadata/DruidSegmentMetadataQuery.java
@@ -16,14 +16,48 @@
 
 package in.zapr.druid.druidry.query.metadata;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import in.zapr.druid.druidry.dataSource.DataSource;
+import in.zapr.druid.druidry.query.QueryType;
+import in.zapr.druid.druidry.query.config.Interval;
+import in.zapr.druid.druidry.query.scan.ResultFormat;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NonNull;
 
+import java.util.List;
+
+@Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @EqualsAndHashCode(callSuper = true)
+public
 class DruidSegmentMetadataQuery extends DruidMetadataQuery {
 
-    public String intervals;
+    public List<Interval> intervals;
     public String toInclude;
     public String merge;
     public String analysisTypes;
     public String lenientAggregatorMerge;
+    private ResultFormat resultFormat;
+
+    @Builder
+    public DruidSegmentMetadataQuery(
+            List<Interval> intervals,
+            String toInclude,
+            String merge,
+            String analysisTypes,
+            String lenientAggregatorMerge,
+            ResultFormat resultFormat,
+            @NonNull DataSource dataSource
+    ) {
+        this.queryType = QueryType.SEGMENT_METADATA;
+        this.dataSource = dataSource;
+        this.intervals = intervals;
+        this.toInclude = toInclude;
+        this.merge = merge;
+        this.analysisTypes = analysisTypes;
+        this.lenientAggregatorMerge = lenientAggregatorMerge;
+        this.resultFormat = resultFormat;
+    }
 }
